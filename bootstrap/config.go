080db6a369b2615e3574c01803624cb77006734e
Developer <dev@example.com>
2026-07-02 15:13:34 +0800

    配置修改

--- bootstrap/config.go
+++ bootstrap/config.go
@@ -7,12 +7,19 @@ import (
 	"gin-biz-web-api/global"
 	pkgConfig "gin-biz-web-api/pkg/config"
 	"gin-biz-web-api/pkg/console"
+
+	"github.com/joho/godotenv"
 )
 
 // setupConfig 初始化配置文件信息
 func setupConfig() {
 
 	console.Info("init config ...")
+
+	// 加载 .env 文件（如果存在）
+	if err := godotenv.Load(); err != nil {
+		console.Warning(".env file not found, using system environment variables")
+	}
 
 	// 通过匿名加载的方式自动加载了 config 包中所有的 init 函数
 
