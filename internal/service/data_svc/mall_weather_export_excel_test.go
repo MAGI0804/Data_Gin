95ba8b8b52afc6be181a3101e86ba8cda20c3f81
magi <magi0804@163.com>
2026-07-27 14:47:42 +0800

    fix: export daily pressure values as numbers

--- internal/service/data_svc/mall_weather_export_excel_test.go
+++ internal/service/data_svc/mall_weather_export_excel_test.go
@@ -89,7 +89,8 @@ func TestMallWeatherExportExcelPercentAndDateFormatting(t *testing.T) {
 func TestMallWeatherExportComprehensiveDecimalFieldsRemainNumeric(t *testing.T) {
 	for _, field := range []string{
 		"visibility_km", "nearest_precip_distance_km", "dswrf_w_m2", "cloudrate_avg_ratio",
-		"day_precipitation_avg_mm_h", "night_wind_avg_direction_deg", "aqi_avg_usa",
+		"day_precipitation_avg_mm_h", "night_wind_avg_direction_deg", "pressure_max_pa",
+		"pressure_min_pa", "pressure_avg_pa", "aqi_avg_usa",
 	} {
 		value, numeric, err := mallWeatherExportNumericValue(field, "general", "metric", "12.5")
 		if err != nil || !numeric || value != 12.5 {
