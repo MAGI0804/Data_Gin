8d65285b13411e3e82a415924c79d2622150b147
magi <magi0804@163.com>
2026-08-25 15:52:17 +0800

    修复中文查询名称浏览器校验

--- web/src/reportCenter/inputQueryName.ts
+++ web/src/reportCenter/inputQueryName.ts
@@ -1,6 +1,6 @@
-export const reportInputQueryNamePatternSource = String.raw`\p{L}[\p{L}\p{N}_-]{0,63}`
+export const reportInputQueryNamePatternSource = String.raw`\p{L}[\p{L}\p{N}_\-]{0,63}`
 
-const reportInputQueryNamePattern = new RegExp(`^(?:${reportInputQueryNamePatternSource})$`, 'u')
+const reportInputQueryNamePattern = /^\p{L}[\p{L}\p{N}_-]{0,63}$/u
 
 export function isReportInputQueryName(value: string) {
 	return reportInputQueryNamePattern.test(value)
